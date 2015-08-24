10e103d42a4ec8108dfcee1ca67bfa2b08e531ba
Troy <troyleak@users.noreply.github.com>
2015-08-23 18:02:38 -0700

    Debugged Part 2
    Found errors in list processing and handling of indices

--- Homework/Week1Part2.py
+++ Homework/Week1Part2.py
@@ -1,25 +1,44 @@
 ###########################################
 # Assignment 2 is similar, so I'm lumping it into one file. I'll break them down
 # later, I'm sure.
+
+def letterAdder(tempList):
+    for i in tempList:
+        if i in hexDict.keys():
+            print "Replacing " + str(i) + " with " + hexDict[i]
+            z = tempList.index(i)
+            y = hexDict[i]
+            finalList.insert(z,y)
+            finalList.remove(i)
+            print finalList
+
 userInput = 0
+
 while True:
     try:
-        userInput = int(input("Enter something: "))
+        userInput = int(input("Enter an integer: "))
     except ValueError:
         print "Not an integer!"
         continue
     else:
-        print "Integer found!"
+        print "That is a valid integer"
         break
 
+finalList = []
+hexDict = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+
+
 while True:
-    # Creates a list with decimal values representing hex values
-    # Need to create a function to convert 10-15 to A-F
-    finalList = []
-    finalList.append(integer%16)
-    print "Appending " + str(integer%16) + " to finalList"
-    integer = (integer/16)
-    print str(integer)
-    if (integer/16)==0:
-        print "Exiting loop"
+    finalList.append(userInput%16)
+    print "Appending " + str(userInput%16) + " to finalList"
+    userInput = (userInput/16)
+    print str(userInput)
+    if (userInput/16)==0:
+        finalList.append(userInput%16)
+        print (str(finalList)) + " are the numbers we're working with"
+        print "Now we need to convert 10 - 15 to A - F"
         break
+
+
+
+letterAdder(finalList)
